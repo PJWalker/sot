31ce77bbff92d99456f3e31cede58784d755065c
PJ Walker <497242+PJWalker@users.noreply.github.com>
2025-04-10 21:56:20 +1000

    vault update: 2025-04-10 21:56:20
    Affected files:
    quartz/plugins/transformers/findSocialImage.tsx

---
+++ quartz/plugins/transformers/findSocialImage.tsx
@@ -0,0 +1,27 @@
+import { MdastRoot } from "mdast-util-to-hast/lib/handlers/root";
+import { QuartzTransformerPlugin } from "../types";
+import { VFile } from "vfile";
+import { visit } from "unist-util-visit";
+
+export const findSocialImage: QuartzTransformerPlugin = () => {
+  return {
+    name: "FindSocialImage",
+    markdownPlugins() {
+      return [
+        () => {
+          return (tree: MdastRoot, file: VFile) => {
+            visit
+          }
+        },
+      ]
+    },
+  }
+}
+
+// tell typescript about our custom data fields we are adding
+// other plugins will then also be aware of this data field
+declare module "vfile" {
+  interface DataMap {
+    ogImage: string
+  }
+}

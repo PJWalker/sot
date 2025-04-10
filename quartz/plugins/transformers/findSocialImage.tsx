8f5c7becb62b4f1c01c5af11860c3bc7033c3436
PJ Walker <497242+PJWalker@users.noreply.github.com>
2025-04-10 22:16:40 +1000

    vault update: 2025-04-10 22:16:40
    Affected files:
    quartz/plugins/transformers/findSocialImage.tsx

--- quartz/plugins/transformers/findSocialImage.tsx
+++ quartz/plugins/transformers/findSocialImage.tsx
@@ -1,29 +1,25 @@
 import { MdastRoot } from "mdast-util-to-hast/lib/handlers/root";
 import { QuartzTransformerPlugin } from "../types";
 import { VFile } from "vfile";
-import { visit } from "unist-util-visit";
+import { EXIT, visit } from "unist-util-visit";
+import { transformInternalLink, transformLink } from "../../util/path";
 
 export const FindSocialImage: QuartzTransformerPlugin = () => {
   return {
     name: "FindSocialImage",
     markdownPlugins() {
       return [
-        // () => {
-        //   return (tree: MdastRoot, file: VFile) => {
-        //     visit(tree, "image", (image) => {console.log(image.url) })
-        //   }
-        // },
+        () => {
+          return (tree: MdastRoot, file: VFile) => {
+            visit(tree, "image", (image) => {
+
+              console.log(transformLink(image.url));
+              return EXIT;
+            })
+          }
+        },
       ]
     },
-    htmlPlugins() {
-      return [
-        () => {
-          return (tree, file: VFile) => {
-            console.log(file.data.links)
-          }
-        }
-      ]
-    }
   }
 }
 
